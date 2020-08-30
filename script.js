9188468af1a0c2b455245f74b8b4d3bc355611d9
programmertaha <programmer.taha.1y@gmail.com>
2020-08-30 09:21:34 +0300

    Second add

---
+++ script.js
@@ -0,0 +1,6 @@
+//convert Kelvin to Celsius, then to Fahrenheit
+//https://s3.amazonaws.com/codecademy-content/projects/introduction-to-javascript/learn-javascript-introduction/kelvin-weather/Kelvin+Thermometers.svg
+
+//today's temperature in kelvin
+const kelvin = 293;
+
