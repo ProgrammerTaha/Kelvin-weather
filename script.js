d7ae7820b57c06a8e82ef89562e82c36c66c01e3
programmertaha <programmer.taha.1y@gmail.com>
2020-08-30 09:24:08 +0300

    Third add

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-//convert Kelvin to Celsius, then to Fahrenheit
-//https://s3.amazonaws.com/codecademy-content/projects/introduction-to-javascript/learn-javascript-introduction/kelvin-weather/Kelvin+Thermometers.svg
+/*
+convert Kelvin to Celsius, then to Fahrenheit
+https://s3.amazonaws.com/codecademy-content/projects/introduction-to-javascript/learn-javascript-introduction/kelvin-weather/Kelvin+Thermometers.svg
+Celsius is 273 degrees less than Kelvin
+*/
 
 //today's temperature in kelvin
 const kelvin = 293;
 
+//convert Kelvin to Celsius
+const celsius = kelvin - 293;
